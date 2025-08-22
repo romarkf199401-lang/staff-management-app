34862d8c269c8f169e2444f76e9bc3d765aeeca3
romarkf199401-lang <romarkf199401@gmail.com>
2025-08-22 12:20:59 +0800

    Commit4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
 import EmployeeList from "./EmployeeList";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+import TimeInOut from "./components/Attendance/TimeInOut";
+import AttendanceRecord from "./components/Attendance/AttendanceRecord";
+import LeaveManagement from "./pages/LeaveManagement";
+
 
 function App() {
   return (
